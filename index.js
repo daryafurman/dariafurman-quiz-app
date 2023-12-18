a3f7a34b70059451924f9a61f928891f4a7f852d
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-18 14:35:21 +0100

    answer is showing

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const questionCards = document.querySelectorAll('[data-js="question-card"]');
+
+  questionCards.forEach(function (card) {
+    const answerButton = card.querySelector('[data-js="answer-button"]');
+    const answerText = card.querySelector(".answer");
+    let isAnswerVisible = false;
+
+    answerButton.addEventListener("click", function () {
+      isAnswerVisible = !isAnswerVisible;
+      answerText.classList.toggle("hidden", !isAnswerVisible);
+      answerButton.textContent = isAnswerVisible ? "Hide Answer" : "Answer";
+    });
+  });
+});
