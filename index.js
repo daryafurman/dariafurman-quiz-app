db57ddcda16abcd608605f8ac4b8f3b50daff1cb
furmandarya1@gmail.com <furmandarya1@gmail.com>
2023-12-18 13:38:59 +0100

    bookmark is working

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const bookmark = document.querySelector('[data-js="bookmark-button]');
+console.clear();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+  let isBookmarked = false;
+
+  bookmarkButton.addEventListener("click", function () {
+    isBookmarked = !isBookmarked;
+    if (isBookmarked) {
+      bookmarkButton.src = "img/red-bookmark.png";
+    } else {
+      bookmarkButton.src = "img/empty-bookmark.webp";
+    }
+  });
+});
